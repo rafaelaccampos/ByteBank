2a3355c7c72b4edb3087fc49f0055dd180ed5dd2
Rafaela Cassiano Campos <rafaela.cassiano@lambda3.com.br>
2022-04-23 22:24:29 -0300

    feat: Retrieving data from component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Transference } from './transference';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ByteBank';
+
+  transfer($event: Transference){
+    console.log($event);
+  }
 }
